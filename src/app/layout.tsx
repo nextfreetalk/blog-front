6a94764cbdc54f44be68f60f3d718e0ce2f963fd
nearworld <artemismars2@outlook.com>
2024-09-27 21:41:10 +0900

    fix: 폰트 파일 css import문으로 다운로드

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,20 +2,20 @@ import type { Metadata } from "next";
 import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
 
-const notoSansKr = Noto_Sans_KR({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  variable: "--font-noto-sans",
-});
+// const notoSansKr = Noto_Sans_KR({
+//   subsets: ["latin"],
+//   weight: ["400", "700"],
+//   variable: "--font-noto-sans",
+// });
 
 export const metadata = {
-  title: 'FreeTalk Blog',
-  description: 'FreeTalk Blog',
+  title: "FreeTalk Blog",
+  description: "FreeTalk Blog",
   // 여기서 메타 태그 추가
   other: {
-    'google-site-verification': 'X6YdZHrWhdKJWPCUe0LrZY93FkFf9X1SvQIsrNgwt38',
+    "google-site-verification": "X6YdZHrWhdKJWPCUe0LrZY93FkFf9X1SvQIsrNgwt38",
   },
-}
+};
 
 export default function RootLayout({
   children,
@@ -24,8 +24,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${notoSansKr.className}`}>{children}</body>
-      {/* <body>{children}</body> */}
+      {/* <body className={`${notoSansKr.className}`}>{children}</body> */}
+      <body>{children}</body>
     </html>
   );
 }
