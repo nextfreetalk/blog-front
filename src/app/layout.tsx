23c9e736cf8249ea439514021c52a9f8485ce49c
nearworld <artemismars2@outlook.com>
2024-09-28 10:13:15 +0900

    fix: prettierrc에 tab 사용 적용 및 각 파일에 새 prettier 세팅 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,28 +2,28 @@ import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
 
 const notoSansKr = Noto_Sans_KR({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  variable: "--font-noto-sans",
+	subsets: ["latin"],
+	weight: ["400", "700"],
+	variable: "--font-noto-sans",
 });
 
 export const metadata = {
-  title: "FreeTalk Blog",
-  description: "프론트엔드 기술 수치 비교 블로그",
-  // 여기서 메타 태그 추가
-  other: {
-    "google-site-verification": "X6YdZHrWhdKJWPCUe0LrZY93FkFf9X1SvQIsrNgwt38",
-  },
+	title: "FreeTalk Blog",
+	description: "프론트엔드 기술 수치 비교 블로그",
+	// 여기서 메타 태그 추가
+	other: {
+		"google-site-verification": "X6YdZHrWhdKJWPCUe0LrZY93FkFf9X1SvQIsrNgwt38",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${notoSansKr.className}`}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${notoSansKr.className}`}>{children}</body>
+		</html>
+	);
 }
