cef7476f937cb3b86e321e91a09565c31384d6a1
oh <guruwang2@gmail.com>
2024-10-01 14:30:38 +0900

    fix : md 파일에 jsonld 옮김

--- src/app/book/[...id]/page.tsx
+++ src/app/book/[...id]/page.tsx
@@ -4,50 +4,24 @@ import "github-markdown-css";
 import PageClient from "./PageClient"; // Import the client-side component
 
 const Page = async () => {
-  const res = await fetch("https:test.ohbital.com/api/content?filename=book/오브젝트.md");
-  const data  = await res.json();
-  const { content, frontmatter } = await compileMDX({
-    source: data.data,
-    options: {
-      parseFrontmatter: true,
-      mdxOptions: {
-        remarkPlugins: [],
-        rehypePlugins: [],
+  const res = await fetch(
+    "http://localhost:3000/api/content?filename=book/오브젝트.md",
+  );
+  const data = await res.json();
+  const { content, frontmatter }: { content: any; frontmatter: any } =
+    await compileMDX({
+      source: data.data,
+      options: {
+        parseFrontmatter: true,
+        mdxOptions: {
+          remarkPlugins: [],
+          rehypePlugins: [],
+        },
       },
-    },
-  });
+    });
 
-  console.log("design : ",content);
-
-  const structuredData = {
-    "@context": "https://schema.org",
-    "@type": "Review",
-    author: {
-      "@type": "Person",
-      name: "oh",
-    },
-    datePublished: "2024-09-28",
-    reviewBody: frontmatter?.desc,
-    reviewRating: {
-      "@type": "Rating",
-      ratingValue: "5",
-      bestRating: "5",
-    },
-    itemReviewed: {
-      "@type": "Book",
-      name: "오브젝트",
-      author: {
-        "@type": "Person",
-        name: "조영호",
-      },
-      isbn: "9791158391409",
-      publisher: {
-        "@type": "Organization",
-        name: "위키북스",
-      },
-      datePublished: "2019-06-17",
-    },
-  };
+  console.log("design : ", content);
+  console.log("jsonLD : ", frontmatter?.structuredData);
 
   return (
     <>
@@ -55,7 +29,9 @@ const Page = async () => {
       <meta name="description" content={frontmatter?.desc} />
       <script
         type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
+        dangerouslySetInnerHTML={{
+          __html: frontmatter?.structuredData,
+        }}
       />
       <PageClient content={content} frontmatter={frontmatter} />
     </>
